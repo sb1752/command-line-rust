de0ea3e204f0d9eb33600b22043a77e34a9a5ef7
Shaan Batra <sb1752@gmail.com>
2022-02-21 19:53:52 -0800

    Rust implementation of grep

---
+++ grepr/src/lib.rs
@@ -0,0 +1,269 @@
+use clap::{App, Arg};
+use regex::{Regex, RegexBuilder};
+use std::{
+    error::Error,
+    fs::{self, File},
+    io::{self, BufRead, BufReader},
+};
+use walkdir::{DirEntry, WalkDir};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    pattern: Regex,
+    files: Vec<String>,
+    recursive: bool,
+    count: bool,
+    invert_match: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("grepr")
+        .version("0.1.0")
+        .author("Shaan Batra")
+        .about("Rust grep")
+        .arg(
+            Arg::with_name("pattern")
+                .value_name("PATTERN")
+                .help("Search pattern")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("file")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("count")
+                .help("Count occurrences")
+                .short("c")
+                .long("count")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("insensitive")
+                .help("Case-insensitive")
+                .short("i")
+                .long("insensitive")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("invert-match")
+                .help("Invert match")
+                .short("v")
+                .long("invert-match")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("recursive")
+                .help("Recursive search")
+                .short("r")
+                .long("recursive")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let input = matches.value_of("pattern").unwrap();
+    let pattern = RegexBuilder::new(input)
+        .case_insensitive(matches.is_present("insensitive"))
+        .build()
+        .map_err(|_| format!("Invalid pattern \"{}\"", input))?;
+
+    Ok(Config {
+        pattern,
+        files: matches.values_of_lossy("file").unwrap(),
+        recursive: matches.is_present("recursive"),
+        count: matches.is_present("count"),
+        invert_match: matches.is_present("invert-match"),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let entries = find_files(&config.files, config.recursive);
+    let show_paths: bool = entries.len() > 1;
+
+    for entry in entries {
+        match entry {
+            Err(e) => eprintln!("{}", e),
+            Ok(filepath) => match open(&filepath) {
+                Err(e) => eprintln!("{}: {}", filepath, e),
+                Ok(file) => {
+                    let matches = find_lines(file, &config.pattern, config.invert_match)?;
+                    if config.count {
+                        if show_paths {
+                            println!("{}:{}", filepath, matches.len());
+                        } else {
+                            println!("{}", matches.len());
+                        }
+                    } else {
+                        for line in matches {
+                            if show_paths {
+                                print!("{}:", filepath);
+                            }
+                            print!("{}", line);
+                        }
+                    }
+                }
+            },
+        }
+    }
+
+    Ok(())
+}
+
+fn find_files(paths: &[String], recursive: bool) -> Vec<MyResult<String>> {
+    let mut results = vec![];
+
+    for path in paths {
+        match path.as_str() {
+            "-" => results.push(Ok(path.to_string())),
+            _ => match fs::metadata(path) {
+                Ok(metadata) => {
+                    if metadata.is_dir() {
+                        if recursive {
+                            for entry in WalkDir::new(path)
+                                .into_iter()
+                                .flatten()
+                                .filter(|e| e.file_type().is_file())
+                            {
+                                results.push(Ok(entry.path().display().to_string()));
+                            }
+                        } else {
+                            results.push(Err(From::from(format!("{} is a directory", path))));
+                        }
+                    } else if metadata.is_file() {
+                        results.push(Ok(path.to_string()))
+                    }
+                }
+                Err(e) => results.push(Err(From::from(format!("{}: {}", path, e)))),
+            },
+        }
+    }
+
+    results
+}
+
+fn find_lines<T: BufRead>(
+    mut file: T, // Trait bound, type must implement BufRead trait. Same as `impl BufRead`.
+    pattern: &Regex,
+    invert_match: bool,
+) -> MyResult<Vec<String>> {
+    let mut results = vec![];
+    let mut buffer = String::new();
+
+    // preserve line endings so loop until EOF reached
+    loop {
+        let bytes = file.read_line(&mut buffer)?;
+        if bytes == 0 {
+            break;
+        }
+
+        let line = buffer.clone();
+
+        if invert_match {
+            if !pattern.is_match(&line) {
+                results.push(line);
+            }
+        } else if pattern.is_match(&line) {
+            results.push(line);
+        }
+
+        buffer.clear();
+    }
+    Ok(results)
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::{find_files, find_lines};
+    use rand::{distributions::Alphanumeric, Rng};
+    use regex::{Regex, RegexBuilder};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let files = find_files(&["./tests/inputs/fox.txt".to_string()], false);
+        assert_eq!(files.len(), 1);
+        assert_eq!(files[0].as_ref().unwrap(), "./tests/inputs/fox.txt");
+
+        // The function should reject a directory without the recursive option
+        let files = find_files(&["./tests/inputs".to_string()], false);
+        assert_eq!(files.len(), 1);
+        if let Err(e) = &files[0] {
+            assert_eq!(e.to_string(), "./tests/inputs is a directory");
+        }
+
+        // Verify the function recurses to find four files in the directory
+        let res = find_files(&["./tests/inputs".to_string()], true);
+        let mut files: Vec<String> = res
+            .iter()
+            .map(|r| r.as_ref().unwrap().replace("\\", "/"))
+            .collect();
+        files.sort();
+        assert_eq!(files.len(), 4);
+        assert_eq!(
+            files,
+            vec![
+                "./tests/inputs/bustle.txt",
+                "./tests/inputs/empty.txt",
+                "./tests/inputs/fox.txt",
+                "./tests/inputs/nobody.txt"
+            ]
+        );
+
+        // Generate a random string to represent a nonexistent file
+        let bad: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        // Verify that the function returns the bad file as an error
+        let files = find_files(&[bad], false);
+        assert_eq!(files.len(), 1);
+        assert!(files[0].is_err());
+    }
+
+    #[test]
+    fn test_find_lines() {
+        let text = b"Lorem\nIpsum\r\nDOLOR";
+
+        // The pattern _or_ should match the one line, "Lorem"
+        let re1 = Regex::new("or").unwrap();
+        let matches = find_lines(Cursor::new(&text), &re1, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+
+        // When inverted, the function should match the other two lines
+        let matches = find_lines(Cursor::new(&text), &re1, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+
+        // This regex will be case-insensitive
+        let re2 = RegexBuilder::new("or")
+            .case_insensitive(true)
+            .build()
+            .unwrap();
+
+        // The two lines "Lorem" and "DOLOR" should match
+        let matches = find_lines(Cursor::new(&text), &re2, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+
+        // When inverted, the remaining line should match
+        let matches = find_lines(Cursor::new(&text), &re2, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+    }
+}
